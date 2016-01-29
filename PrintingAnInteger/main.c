dd7061a76ae79c8579c74ba7e32096ba90f951db
Premdeep Amudala <preamdhep.usapp@gmail.com>
2016-01-28 23:16:09 -0800

    Added Printing an Integer Program in C

---
+++ PrintingAnInteger/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Program to print an integer using Scanf() function.
+
+int main()
+{
+    int a;
+    printf("Enter an Integer\n");
+    scanf("%d",&a);
+    printf("The integer entered is %d",a);
+
+    return 0;
+}
