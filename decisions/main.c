a4fb2931b528d398157a2a2f6d81810698ca1a08
User_Account <User_Account@sci6-80.sfsu.edu>
2016-02-26 16:24:37 -0800

    Added decisions program

---
+++ decisions/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("enter a: \n");
+    scanf("%d",&a);
+
+    if(  a%2 == 0  ){
+        printf("even\n");
+    }else{
+
+    printf("odd\n");
+    }
+
+    printf("At the end\n");
+
+
+
+
+
+    return 0;
+}
